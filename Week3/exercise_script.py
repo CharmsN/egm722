933864595cdafee6887ab355266c7da429bdd269
CharmsN <Newmarch-C@Ulster.ac.uk>
2023-04-03 17:58:51 +0100

    Revert to backup

--- Week3/exercise_script.py
+++ Week3/exercise_script.py
@@ -1,23 +1,45 @@
-import geopandas as gpd
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from cartopy.feature import ShapelyFeature
-import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
-
+import geopandas as gpd
+import cartopy.crs as ccrs
 
 # ---------------------------------------------------------------------------------------------------------------------
 # in this section, write the script to load the data and complete the main part of the analysis.
 # try to print the results to the screen using the format method demonstrated in the workbook
 
 # load the necessary data here and transform to a UTM projection
+# load Data Files
+counties = gpd.read_file('data_files/Counties.shp')
+wards = gpd.read_file('data_files/NI_Wards.shp')
 
-# your analysis goes here...
-
-# ---------------------------------------------------------------------------------------------------------------------
-# below here, you may need to modify the script somewhat to create your map.
-# create a crs using ccrs.UTM() that corresponds to our CRS
+# target crs to use
 myCRS = ccrs.UTM(29)
+
+# transform to UTM projection
+counties_utm = counties.to_crs(myCRS)
+wards_utm = wards.to_crs(myCRS)
+
+# set up the plot axes and plot the county polygon (boundary only)
+ax=counties_utm.plot(fc='none')
+
+#create a new gpd which is a copy of wards_utm so that that my original gpd remains unchanged
+wards_reppoint = wards_utm.copy()
+
+#create representative points for the ward polygons and plot on existing axes
+wards_reppoint['geometry'] = wards_reppoint['geometry'].representative_point()
+wards_reppoint.plot(ax=ax)
+
+#perform a spatial join on counties_item and wards_reppoint
+join = gpd.sjoin(counties_utm, wards_reppoint, how='inner', lsuffix='left', rsuffix='right')
+join.shape
+
+join_sum_county = join.groupby(['CountyName'])['Population'].sum()
+join_sum_ward = join.groupby(['CountyName','Ward'])['Population'].sum()
+print(join_sum_county)
+print(join_sum_ward)
+
 # create a figure of size 10x10 (representing the page size in inches
 fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=myCRS))
 
@@ -33,10 +55,10 @@ divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
 
 # plot the ward data into our axis, using
-ward_plot = wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
+ward_plot = wards_utm.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
                        legend=True, cax=cax, legend_kwds={'label': 'Resident Population'})
 
-county_outlines = ShapelyFeature(counties['geometry'], myCRS, edgecolor='r', facecolor='none')
+county_outlines = ShapelyFeature(counties_utm['geometry'], myCRS, edgecolor='r', facecolor='none')
 
 ax.add_feature(county_outlines)
 county_handles = [mpatches.Rectangle((0, 0), 1, 1, facecolor='none', edgecolor='r')]
